e75baa09ae64692cf70771c7bf404e5514b1c9a2
lenurseytasanov <lenur.seytasanov@mail.ru>
2023-10-15 13:09:49 +0500

    Task 1 complete

---
+++ src/main/java/edu/hw2/Expr.java
@@ -0,0 +1,91 @@
+package edu.hw2;
+
+public sealed interface Expr {
+    double evaluate();
+
+    public record Constant(double x) implements Expr {
+        public double evaluate() {
+            return x;
+        }
+    }
+
+    public record Negate(Expr x, Expr y) implements Expr {
+        public Negate(Expr y) {
+            this(new Constant(0), y);
+        }
+
+        public Negate(double y) {
+            this(new Constant(y));
+        }
+
+        public Negate(double x, double y) {
+            this(new Constant(x), new Constant(y));
+        }
+
+        public Negate(Expr x, double y) {
+            this(x, new Constant(y));
+        }
+
+        public Negate(int x, Expr y) {
+            this(new Constant(x), y);
+        }
+
+        public double evaluate() {
+            return x.evaluate() - y.evaluate();
+        }
+    }
+
+    public record Exponent(Expr x, Expr y) implements Expr {
+        public Exponent(double x, double y) {
+            this(new Constant(x), new Constant(y));
+        }
+
+        public Exponent(Expr x, double y) {
+            this(x, new Constant(y));
+        }
+
+        public Exponent(double x, Expr y) {
+            this(new Constant(x), y);
+        }
+
+        public double evaluate() {
+            return Math.pow(x.evaluate(), y.evaluate());
+        }
+    }
+
+    public record Addition(Expr x, Expr y) implements Expr {
+        public Addition(double x, double y) {
+            this(new Constant(x), new Constant(y));
+        }
+
+        public Addition(Expr x, double y) {
+            this(x, new Constant(y));
+        }
+
+        public Addition(double x, Expr y) {
+            this(new Constant(x), y);
+        }
+
+        public double evaluate() {
+            return x.evaluate() + y.evaluate();
+        }
+    }
+
+    public record Multiplication(Expr x, Expr y) implements Expr {
+        public Multiplication(double x, double y) {
+            this(new Constant(x), new Constant(y));
+        }
+
+        public Multiplication(Expr x, double y) {
+            this(x, new Constant(y));
+        }
+
+        public Multiplication(double x, Expr y) {
+            this(new Constant(x), y);
+        }
+
+        public double evaluate() {
+            return x.evaluate() * y.evaluate();
+        }
+    }
+}
---
+++ src/test/java/edu/hw2/Task1Test.java
@@ -0,0 +1,30 @@
+package edu.hw2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import edu.hw2.Expr.*;
+
+public class Task1Test {
+    public final static double DELTA = 0.01;
+
+    @Test
+    public void test1() {
+        var two = new Constant(2);
+        var four = new Constant(4);
+        var negOne = new Negate(new Constant(1));
+        var sumTwoFour = new Addition(two, four);
+        var mult = new Multiplication(sumTwoFour, negOne);
+        var exp = new Exponent(mult, 2);
+        var res = new Addition(exp, new Constant(1));
+        assertEquals(37, res.evaluate(), DELTA);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(2 + 2 * 2, new Addition(2, new Multiplication(2, 2)).evaluate(), DELTA);
+        assertEquals(
+            23.5 * (5.3 - 3.2) + Math.pow(3.2, 1.2),
+            new Addition(new Multiplication(23.5, new Negate(5.3, 3.2)), new Exponent(3.2, 1.2)).evaluate(),
+            DELTA);
+    }
+}
